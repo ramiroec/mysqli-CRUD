cbb570b24da44ed839a3809492c6c8776f5bea61
Ramiro Estigarribia Canese <ramiroec@hotmail.com>
2024-04-12 22:53:29 -0400

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,9 +1,9 @@
 <?php
-$servername = "139.59.58.139"; 
+$servername = "localhost"; 
 // Nombre del servidor de la base de datos
-$port = 27723; // Puerto de la base de datos
-$username = "avnadmin"; // Nombre de usuario de la base de datos
-$password = "AVNS_jfvSePC1yFQ2_dYmcbD"; // Contraseña de la base de datos
+$port = 3306; // Puerto de la base de datos
+$username = "root"; // Nombre de usuario de la base de datos
+$password = ""; // Contraseña de la base de datos
 $dbname = "defaultdb"; // Nombre de la base de datos
 
 // Establecer la conexión
